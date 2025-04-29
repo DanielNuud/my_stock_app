4094105f8081567fec481fb1700468472357a06a
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-04-29 09:04:27 +0300

    create company-info-service

---
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/CompanyInfoServiceApplication.java
@@ -0,0 +1,13 @@
+ package daniel.nuud.company_info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyInfoServiceApplication.class, args);
+    }
+
+}
---
+++ company-info-service/src/test/java/daniel/nuud/company_info_service/CompanyInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.nuud.company_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
