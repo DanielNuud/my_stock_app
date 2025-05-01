7a3ab0461167b77e4ecb56b03582b677192f2b32
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-01 19:50:55 +0300

    fix

--- company-info-service/src/main/java/daniel/nuud/company_info_service/dto/Ticket.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/dto/Ticket.java
@@ -1,15 +1,27 @@
 package daniel.nuud.company_info_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Ticket {
+
     private String ticker;
+
     private Branding branding;
+
     private Address address;
+
     private String description;
+
+    @JsonProperty("homepage_url")
     private String homepageUrl;
+
     private String name;
+
+    @JsonProperty("primary_exchange")
     private String primaryExchange;
+
+    @JsonProperty("market_cap")
     private Long marketCap;
 }
