f83b7a1395ee410ecae27d1ae5fa69babbec7673
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-02 12:21:57 +0300

    update

--- company-info-service/src/main/java/daniel/nuud/company_info_service/service/CompanyService.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/service/CompanyService.java
@@ -2,6 +2,7 @@ package daniel.nuud.company_info_service.service;
 
 import daniel.nuud.company_info_service.dto.ApiResponse;
 import daniel.nuud.company_info_service.dto.Ticket;
+import daniel.nuud.company_info_service.exception.ResourceNotFoundException;
 import daniel.nuud.company_info_service.model.Company;
 import daniel.nuud.company_info_service.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -39,6 +42,8 @@ public class CompanyService {
                         company.setMarketCap(data.getMarketCap());
                         company.setPrimaryExchange(data.getPrimaryExchange());
 
+                        company.setStatus(response.getStatus());
+
                         return Mono.fromCallable(() -> companyRepository.save(company));
                     } else {
                         return Mono.error(new RuntimeException("Company not found"));
@@ -47,7 +52,7 @@ public class CompanyService {
                 });
     }
 
-    public Mono<Company> getCompanyByTicker(String ticker) {
-        return Mono.justOrEmpty(companyRepository.findById(ticker));
+    public Company getCompanyByTicker(String ticker) {
+        return companyRepository.findById(ticker).orElseThrow(() -> new ResourceNotFoundException("Company not found!"));
     }
 }
