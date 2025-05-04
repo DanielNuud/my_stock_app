f6daddde047d060e21e4267ebaa4c58cc2f8df7d
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-04 10:49:42 +0300

    create controller

---
+++ news-service/src/main/java/daniel/nuud/newsservice/controller/NewsController.java
@@ -0,0 +1,26 @@
+package daniel.nuud.newsservice.controller;
+
+import daniel.nuud.newsservice.model.Article;
+import daniel.nuud.newsservice.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+
+    @Autowired
+    private NewsService newsService;
+
+    @GetMapping("/{ticker}")
+    public ResponseEntity<List<Article>> getNews(@PathVariable String ticker) {
+        var Article = newsService.fetchAndSaveNews(ticker);
+        return ResponseEntity.ok(Article);
+    }
+}
