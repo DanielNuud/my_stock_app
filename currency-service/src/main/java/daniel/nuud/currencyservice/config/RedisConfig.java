5499c7eb49695fc15d3fb3692ff0619686d0724e
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-19 08:25:12 +0300

    update

--- currency-service/src/main/java/daniel/nuud/currencyservice/config/RedisConfig.java
+++ currency-service/src/main/java/daniel/nuud/currencyservice/config/RedisConfig.java
@@ -3,16 +3,19 @@ package daniel.nuud.currencyservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues();
+    public RedisCacheManager cacheManager(RedisConnectionFactory cf) {
+        RedisCacheConfiguration cfg = RedisCacheConfiguration.defaultCacheConfig()
+                .computePrefixWith(name -> name + ":");  // ключи вида currency:rates
+        return RedisCacheManager.builder(cf)
+                .cacheDefaults(cfg)
+                .build();
     }
 }
