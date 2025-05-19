e2104ac5782071e416167d95b432ec44f4e3ce48
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-19 08:25:02 +0300

    update

--- company-info-service/src/main/java/daniel/nuud/company_info_service/controller/CompanyController.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/controller/CompanyController.java
@@ -2,7 +2,9 @@ package daniel.nuud.company_info_service.controller;
 
 import daniel.nuud.company_info_service.model.Company;
 import daniel.nuud.company_info_service.service.CompanyService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/companies")
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
 
     @Autowired
@@ -21,9 +24,9 @@ public class CompanyController {
 
 
     @GetMapping("/{ticker}")
-    public ResponseEntity<Company> getCompany(@PathVariable String ticker) {
+    public ResponseEntity<Company> getCompany(@PathVariable String ticker, HttpServletRequest req) {
+        log.info("Received GET " + req.getRequestURI());
         Company company = companyService.fetchCompany(ticker);
-
         return ResponseEntity.ok(company);
     }
 
