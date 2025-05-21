90112443e00d1f528d915617740f6ec7edb7b76e
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-21 16:04:19 +0300

    update

--- historical-analytics-service/src/main/java/daniel/nuud/historicalanalyticsservice/controller/HistoricalController.java
+++ historical-analytics-service/src/main/java/daniel/nuud/historicalanalyticsservice/controller/HistoricalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/api/v1/historical")
+@RequestMapping("/api/historical")
 @RequiredArgsConstructor
 public class HistoricalController {
 
