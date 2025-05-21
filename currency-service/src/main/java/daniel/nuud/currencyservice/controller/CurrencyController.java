d2a6263fe248328419b1082b3bf5508653e99432
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-05-21 16:05:38 +0300

    update

--- currency-service/src/main/java/daniel/nuud/currencyservice/controller/CurrencyController.java
+++ currency-service/src/main/java/daniel/nuud/currencyservice/controller/CurrencyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/currency")
 public class CurrencyController {
 
     private CurrencyService currencyService;
