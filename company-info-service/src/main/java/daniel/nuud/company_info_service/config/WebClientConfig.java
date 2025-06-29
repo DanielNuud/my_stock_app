29cec5f6b787021a4121549d6431ccb7d68c1faf
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-06-29 19:15:48 +0300

    update

--- company-info-service/src/main/java/daniel/nuud/company_info_service/config/WebClientConfig.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/config/WebClientConfig.java
@@ -3,21 +3,35 @@ package daniel.nuud.company_info_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.Map;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient polygonWebClient(@Value("${polygon.api.key}") String apiKey) {
+    public WebClient polygonWebClient(@Value("${POLYGON_API_KEY}") String apiKey) {
         return WebClient.builder()
                 .baseUrl("https://api.polygon.io")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultUriVariables(Map.of("apiKey", apiKey))
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
