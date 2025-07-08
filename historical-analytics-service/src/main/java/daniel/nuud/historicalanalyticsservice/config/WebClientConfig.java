f14566dbb5b7641f8714c38aded21ff37fa6ded8
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-07-08 12:41:02 +0300

    add Corsmapping

--- historical-analytics-service/src/main/java/daniel/nuud/historicalanalyticsservice/config/WebClientConfig.java
+++ historical-analytics-service/src/main/java/daniel/nuud/historicalanalyticsservice/config/WebClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Map;
 
@@ -19,5 +21,17 @@ public class WebClientConfig {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultUriVariables(Map.of("apiKey", apiKey))
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
