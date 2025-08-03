49d81ebc3bad2c4ddb76a4e74f979e6ee0ec0248
Daniel Nüüd <daaniel.nuud@gmail.com>
2025-08-03 21:23:03 +0300

    update

--- company-info-service/src/main/java/daniel/nuud/company_info_service/config/RedisConfig.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/config/RedisConfig.java
@@ -1,11 +1,8 @@
 package daniel.nuud.company_info_service.config;
 
-import daniel.nuud.company_info_service.model.Company;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.Duration;
 
--- company-info-service/src/main/java/daniel/nuud/company_info_service/controller/CompanyController.java
+++ company-info-service/src/main/java/daniel/nuud/company_info_service/controller/CompanyController.java
@@ -5,8 +5,6 @@ import daniel.nuud.company_info_service.service.CompanyService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +16,11 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
-    @Autowired
-    CacheManager cacheManager;
-
     @GetMapping("/{ticker}")
     public ResponseEntity<Company> getCompany(@PathVariable String ticker, HttpServletRequest req) {
         log.info("Received GET " + req.getRequestURI());
         Company company = companyService.fetchCompany(ticker);
         return ResponseEntity.ok(company);
     }
-
-    @DeleteMapping("/clear-cache")
-    public void clearCache() {
-        cacheManager.getCache("Company").clear();
-    }
-
-
 
 }
